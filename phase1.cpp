7ef32d02bc0b917d548afff9d80cef4c9bb7743d
root <root@MX>
2025-04-25 00:52:59 +0330

    004: class sig

--- phase1.cpp
+++ phase1.cpp
@@ -35,6 +35,35 @@ public:
     int getReservation();
 };
 
+class Meal
+{
+    int meal_id;
+    string name;
+    float price;
+//    enum meal_type;
+//    vector side_items;
+public:
+    Meal();
+    void print() const;
+//    void update_price(float new_price);
+//    void add_side_item(str item);
+    void setMeal();
+    int getMeal() const;
+};
+
+class DiningHall
+{
+    int hall_id;
+    string name;
+    string address;
+    int capacity;
+public:
+    DiningHall();
+    void print() const;
+    void setDiningHall();
+    int getDiningHall() const;
+};
+
 int main()
 {
     return 0;
