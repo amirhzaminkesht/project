f31c14e322a7935274dd999c4ddd375703b96d9d
root <root@MX>
2025-04-26 00:51:36 +0330

    005: algo. sign.

--- phase1.cpp
+++ phase1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
 using namespace std;
+
+enum class MealType {BREAKFAST, LUNCH, DINNER};
+enum class ReservationStatus {SUCCESS, CANCELLED, FAILED};
 
 class Student
 {
@@ -9,30 +15,53 @@ class Student
     string email;
     float balance;
     bool is_active;
-
+    vector<Reservation*> reservations;
 public:
-    Student();
+    Student(int user_id, string student_id, string name, string email, float balance, bool is_active);
+
+    int get_user_id() const;
+    string get_student_id() const;
+    string get_name() const;
+    string get_email() const;
+    float get_balance() const;
+    bool get_is_active() const;
+    vector<Reservation*>& get_reservations();
+    
+    void set_user_id(int id);
+    void set_student_id(const string& id);
+    void set_name(const string& name);
+    void set_email(const string& email);
+    void set_balance(float balance);
+    void set_is_active(bool is_active);
+    
+    void reserve_meal(Meal *meal, DiningHall *dininghall);
+    bool cancel_reservation(int index);
     void print() const;
-//    reserve_meal(meal);
-//    bool cancel_reservation(reservation);
-    void setStudent();
-    int getStudent() const;
 };
 
-class Resevation
+class Reservation
 {
     int reservation_id;
-//    Student student;
-//    DiningHall dhall;
-//    enum status;
-//    time_t created_at;
-
+    Student *student;
+    DiningHall *dHall;
+    Meal *meal;
+    ReservationStatus status;
+    time_t created_at;
 public:
-    Resevation();
-    void print() const;
+    Reservation(int reservation_id, Student *student, DiningHall *dHall, Meal *meal);
+    
+    int get_reservation_id() const;
+    Student *get_student() const;
+    DiningHall *get_dHall() const;
+    Meal *get_meal() const;
+    ReservationStatus get_status() const;
+    time_t get_created_at() const;
+
+    void set_reservation_id(int id);
+    void set_status(ReservationStatus status);
+
     bool cancel();
-    void setReservation();
-    int getReservation();
+    void print() const;
 };
 
 class Meal
@@ -40,15 +69,25 @@ class Meal
     int meal_id;
     string name;
     float price;
-//    enum meal_type;
-//    vector side_items;
+    MealType meal_type;
+    vector<string> side_items;
 public:
-    Meal();
+    Meal(int meal_id, string name, float price, MealType meal_type);
+    
+    int get_meal_id() const;
+    string get_name() const;
+    float get_price() const;
+    MealType get_meal_type() const;
+    vector<string> get_side_items() const;
+    
+    void set_meal_id(int id);
+    void set_name(const string &name);
+    void set_price(float price);
+    void set_meal_type(MealType meal_type);
+
+    void update_price(float new_price);
+    void add_side_item(const string& item);
     void print() const;
-//    void update_price(float new_price);
-//    void add_side_item(str item);
-    void setMeal();
-    int getMeal() const;
 };
 
 class DiningHall
@@ -58,10 +97,19 @@ class DiningHall
     string address;
     int capacity;
 public:
-    DiningHall();
+    DiningHall(int hall_id, string name, string address, int capacity);
+
+    int get_hall_id() const;
+    string get_name() const;
+    string get_address() const;
+    int get_capacity() const;
+    
+    void set_hall_id(int id);
+    void set_name(const string& name);
+    void set_address(const string& address);
+    void set_capacity(int capacity);
+    
     void print() const;
-    void setDiningHall();
-    int getDiningHall() const;
 };
 
 int main()
